b125ea28ff2344d68e9c842aab5385ae06a514a5
Nir Adler <niradler55@gmail.com>
2019-03-05 03:06:35 +0200

    changes

---
+++ src/services/helpers.js
@@ -0,0 +1,20 @@
+import Amplify from './Amplify';
+const {Auth} = Amplify;
+
+const currentAuthenticatedUser = async() => Auth.currentAuthenticatedUser();
+
+const getToken = async () => {
+    const session = await Auth.currentSession().catch(e=> {throw e});
+    const token = session.getIdToken().getJwtToken();
+    return token;
+}
+
+const favoritesMap = (favs) =>favs.reduce((favs,fav)=>{
+    favs={...favs,[fav.resourceId]:true}
+    return favs;
+},{})
+
+export  {
+    getToken,
+    favoritesMap
+}
